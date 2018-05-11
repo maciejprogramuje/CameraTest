1a8fcbb7b22dc19b40c357378e87c059f207769f
maciejprogramuje <maciejsz1979@gmail.com>
2018-05-11 12:39:46 +0200

    ok

--- app/src/main/java/com/maciejprogramuje/facebook/cameratest/MainActivity.java
+++ app/src/main/java/com/maciejprogramuje/facebook/cameratest/MainActivity.java
@@ -1,30 +1,28 @@
 package com.maciejprogramuje.facebook.cameratest;
 
-import android.hardware.Camera;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    private Camera mCamera = null;
-    private SurfaceView mSurfaceView = null;
-
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    @BindView(R.id.surface_layout)
-    FrameLayout surfaceLayout;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+    @BindView(R.id.previewImageView)
+    ImageView previewImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +31,6 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        mCamera = Camera.open();
-        if (mCamera != null) {
-            mSurfaceView = new SurfaceView(this, mCamera);
-            surfaceLayout.addView(mSurfaceView);
-        }
     }
 
     @Override
@@ -73,6 +57,23 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.fab)
     public void onViewClicked() {
-        System.exit(0);
+        dispatchTakePictureIntent();
+        Toast.makeText(this, "photo ok!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            previewImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
