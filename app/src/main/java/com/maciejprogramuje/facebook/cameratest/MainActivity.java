8076e8c5e93bdfee71ab5e944ac875b9ceef41a2
maciejprogramuje <maciejsz1979@gmail.com>
2018-05-11 15:41:09 +0200

    ok

--- app/src/main/java/com/maciejprogramuje/facebook/cameratest/MainActivity.java
+++ app/src/main/java/com/maciejprogramuje/facebook/cameratest/MainActivity.java
@@ -2,21 +2,35 @@ package com.maciejprogramuje.facebook.cameratest;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 2;
+    String mCurrentPhotoPath;
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.fab)
@@ -58,22 +72,58 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.fab)
     public void onViewClicked() {
         dispatchTakePictureIntent();
-        Toast.makeText(this, "photo ok!", Toast.LENGTH_SHORT).show();
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 1);
+            try {
+                File photoFile = createImageFile();
+                Uri photoURI = FileProvider.getUriForFile(this, "com.maciejprogramuje.facebook.cameratest", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                galleryAddPic();
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             previewImageView.setImageBitmap(imageBitmap);
+
+            Toast.makeText(this, "Image captured!", Toast.LENGTH_SHORT).show();
         }
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Toast.makeText(this, "Photo taken!\n"+mCurrentPhotoPath, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,  ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.w("UWAGA", "mCurrentPhotoPath: " + mCurrentPhotoPath);
+
+
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
